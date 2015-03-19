a06ca9ea3187ff710513207307b4043707c5a012
1200044@abertay.ac.uk <1200044@abertay.ac.uk>
2015-03-19 15:57:30 +0000

    I've attempted to get the camera to switch to a blank-screen shader to skip frames, however this doesn't address the issue that the game can't achieve the desired frame rate in the first place if the machine running the game is too weak. The shader will still go over every object in the cameras' views and process their geometry. The outcome can charitibly be described as "Epilepsy Mode".
    Signed-off-by: Andrew Robson <1200044@abertay.ac.uk>

---
+++ downloadThisBuild/Assets/BlankShader.shader
@@ -0,0 +1,24 @@
+﻿Shader "Custom/BlankShader" 
+{
+	SubShader 
+	{
+		Pass
+		{
+			CGPROGRAM
+			
+			#pragma vertex vert
+            #pragma fragment frag
+			
+			float4 vert(float4 v:POSITION) : SV_POSITION 
+			{
+                return float4(0.0f, 0.0f, 0.0f, 1.0f);
+            }
+            
+			fixed4 frag() : SV_Target 
+			{
+                return fixed4(0.0,0.0,0.0,1.0);
+            }
+			ENDCG
+		}
+	}
+}
---
+++ downloadThisBuild/Assets/Scripts/CameraBlanker.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Andrew Robson:
+// This script checks the output's frame rate
+
+public class CameraBlanker : MonoBehaviour {
+
+	FPSCounter counter;
+	public bool BlankFrames = true;
+	public float TargetFPS = 59.0f;
+	public int MinimumShownFrames = 3;
+	private int FramesSinceBlanked;
+	
+	public Camera LeftCamera;
+	public Camera RightCamera;
+	
+	public Shader BlankingShader;
+	
+	bool Errors;
+	
+	// Use this for initialization
+	void Start () {
+		counter = GetComponent<FPSCounter>();
+		
+		
+		if (counter == null)
+		{
+			Debug.LogWarning("FPS counter not found!");
+			Errors = true;
+		}
+		
+		if ( (LeftCamera == null) || (RightCamera == null) )
+		{
+			Debug.LogWarning("One or both cameras are not set for blanking.");
+			Errors = true;
+		}
+		// Only check this if both cameras were set;
+		else if (LeftCamera == RightCamera)
+		{
+			Debug.LogWarning("Cameras for blanking are one and the same.");
+			Errors = true;
+		}
+		
+		if (BlankingShader == null)
+		{
+			Debug.LogWarning("The blanking shader has not been set up.");
+			Errors = true;
+		}
+		
+		if (Errors)
+		{
+			Debug.LogWarning("Cameras will not be blanked on slow frames.");
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( (!Errors) && (BlankFrames))
+		{
+			if ( (counter.GetFPS() < TargetFPS) && (FramesSinceBlanked > MinimumShownFrames) )
+			{
+				// Blank the frame.
+				Debug.Log("Frame blanked out!");
+				
+				LeftCamera.SetReplacementShader(BlankingShader, null);
+				RightCamera.SetReplacementShader(BlankingShader, null);
+				
+				LeftCamera.clearFlags = CameraClearFlags.SolidColor;
+				RightCamera.clearFlags = CameraClearFlags.SolidColor;
+				
+				RenderSettings.fog = false;
+				
+				FramesSinceBlanked = 0;
+			}
+			else
+			{
+				// Show the frame
+				LeftCamera.ResetReplacementShader();
+				RightCamera.ResetReplacementShader();
+				
+				LeftCamera.clearFlags = CameraClearFlags.Skybox;
+				RightCamera.clearFlags = CameraClearFlags.Skybox;
+				
+				RenderSettings.fog = true;
+				
+				FramesSinceBlanked++;
+			}
+		}
+	}
+}
