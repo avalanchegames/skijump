8d1620a8e49cabc2435f1378b3a2c012b2d3ed47
1202346 <1202346@abertay.ac.uk>
2015-03-30 12:02:34 +0100

    Signed-off-by: 1202346 <1202346@abertay.ac.uk>

--- downloadThisBuild/Assets/Scripts/LandingTrigger.cs
+++ downloadThisBuild/Assets/Scripts/LandingTrigger.cs
@@ -1,24 +1,26 @@
-﻿using UnityEngine;
+﻿// Changes the player's state when it lands on the slope and plays a sound to indicate that the player has landed.
+
+using UnityEngine;
 using System.Collections;
 
 public class LandingTrigger : MonoBehaviour {
 
 	PlayerStateController PlayerStateManager;
-	public AudioClip landingSound;
-	bool soundTrigger = false;
+	public AudioClip landingSound;	// The sound that will be played when the player collides with the landing trigger
+	bool soundTrigger = false;	// Used to check wether or not the landing sound has played
 	
 
 
-	void OnTriggerEnter( Collider other )
+	void OnTriggerEnter( Collider other )	// If the landing trigger collides with the player.
 	{
-		other.gameObject.GetComponent <PlayerMovement>().landed = true;
+		other.gameObject.GetComponent <PlayerMovement>().landed = true;	// Change the player's state to landed.
 		//PlayerStateManager = gameObject.GetComponent<PlayerStateController> ();
 		//if (PlayerStateManager.getState () == PlayerStateController.playerStates.landing && !soundTrigger) {
-		if (!soundTrigger)
+		if (!soundTrigger) // If the soound has not already been played.
 		{
-			other.gameObject.audio.Stop ();
-			other.gameObject.audio.PlayOneShot (landingSound);
-			soundTrigger = true;
+			other.gameObject.audio.Stop (); // Stop any audio the player is currently playing.
+			other.gameObject.audio.PlayOneShot (landingSound);	// Play the landing sound once.
+			soundTrigger = true;	// Used to ensure the landing sound is only played onbce.
 		}
 	}
 }
--- downloadThisBuild/Assets/Scripts/startSound.cs
+++ downloadThisBuild/Assets/Scripts/startSound.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿// The initial sound when before the player starts their ski jump.
+
+using UnityEngine;
 using System.Collections;
 
 public class startSound : MonoBehaviour {
 
 	PlayerStateController PlayerStateManager;
-	public AudioClip start_sound;
-	bool sound_played = false;
+	public AudioClip start_sound;	// The sound that will beused.
+	bool sound_played = false;	// Used to ensure the sound is only played once.
 	
 	// Use this for initialization
 	void Start () {
@@ -14,12 +16,12 @@ public class startSound : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		PlayerStateManager = gameObject.GetComponent <PlayerStateController> ();
-		if (PlayerStateManager.getState() == PlayerStateController.playerStates.starting && !sound_played) 
+		PlayerStateManager = gameObject.GetComponent <PlayerStateController> ();	// Get the player's current state.
+		if (PlayerStateManager.getState() == PlayerStateController.playerStates.starting && !sound_played) // If the player's state is starting and the start sound has not been played.
 		{
-			audio.clip = start_sound;
-			sound_played = true;
-			audio.Play();
+			audio.clip = start_sound;	// Make the source sound the start sound.
+			sound_played = true;	// Stops the sound from being played again.
+			audio.Play();	// Play the sound in a loop.
 		}
 		
 	}
