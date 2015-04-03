d879358ffb404dad21cc56cf5eb6a5e71c8c3ea4
unknown <1202346@MC10747.uad.ac.uk>
2015-04-03 12:54:17 +0100

    Signed-off-by: unknown <1202346@MC10747.uad.ac.uk>

--- downloadThisBuild/Assets/Scripts/SlowMoTriggerScript.cs
+++ downloadThisBuild/Assets/Scripts/SlowMoTriggerScript.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class SlowMoTriggerScript : MonoBehaviour {
 
+	public AudioClip SoundFile;	// The sound that is played while the player is in slow motion
+
 	void OnTriggerEnter( Collider other )
 	{
 		other.gameObject.GetComponent <PlayerMovement>().slowMo = true;
+		other.gameObject.audio.Stop ();
+		other.gameObject.audio.PlayOneShot (SoundFile);
 	}
 	
 	void OnTriggerExit( Collider other )
