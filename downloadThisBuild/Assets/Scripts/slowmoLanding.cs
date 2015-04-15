b20f8c2601ac27d9eeef0cf26b4f814cb0da69d7
unknown <1202346@MC8196.uad.ac.uk>
2015-04-15 09:44:58 +0100

    Signed-off-by: unknown <1202346@MC8196.uad.ac.uk>

--- downloadThisBuild/Assets/Scripts/slowmoLanding.cs
+++ downloadThisBuild/Assets/Scripts/slowmoLanding.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SlowmoLanding : MonoBehaviour {
-
-	public AudioClip soundFile;	// The sound that is played while the player is in slow motion
-
+public class slowmoLanding : MonoBehaviour {
+	
+	public AudioClip SoundFile;	// The sound that is played while the player is in slow motion
+	
 	void OnTriggerEnter( Collider other )
 	{
 		other.gameObject.GetComponent <PlayerMovement>().slowMo = true;
 		other.gameObject.audio.Stop ();
-		other.gameObject.audio.PlayOneShot (soundFile);
+		other.gameObject.audio.PlayOneShot (SoundFile);
 	}
 }
--- downloadThisBuild/Assets/Scripts/startSound.cs
+++ downloadThisBuild/Assets/Scripts/startSound.cs
@@ -3,20 +3,21 @@
 using UnityEngine;
 using System.Collections;
 
-public class StartSound : MonoBehaviour {
-
-	PlayerStateController playerStateManager;
+public class startSound : MonoBehaviour {
+	
+	PlayerStateController PlayerStateManager;
 	public AudioClip start_sound;	// The sound that will beused.
 	bool sound_played = false;	// Used to ensure the sound is only played once.
 	
 	// Use this for initialization
 	void Start () {
-		playerStateManager = gameObject.GetComponent <PlayerStateController> ();	// Get the player's current state.
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (playerStateManager.GetState() == PlayerStateController.playerStates.starting && !sound_played) // If the player's state is starting and the start sound has not been played.
+		PlayerStateManager = gameObject.GetComponent <PlayerStateController> ();	// Get the player's current state.
+		if (PlayerStateManager.GetState() == PlayerStateController.playerStates.starting && !sound_played) // If the player's state is starting and the start sound has not been played.
 		{
 			audio.clip = start_sound;	// Make the source sound the start sound.
 			sound_played = true;	// Stops the sound from being played again.
