ac6ce8715648acbae9be538dfefc678af7c3183e
1200044@abertay.ac.uk <1200044@abertay.ac.uk>
2015-04-18 16:54:07 +0100

    Restoring deleted files with PascalCase filenames.
    Signed-off-by: Andrew Robson <1200044@abertay.ac.uk>

---
+++ downloadThisBuild/Assets/Scripts/FinishedSound.cs
@@ -0,0 +1,38 @@
+﻿// --- Plays a sound when the player stops at the end of his jump. 
+// Script by Tony Jarvis.
+
+using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices; // Required for realspace3d
+
+public class FinishedSound : MonoBehaviour 
+{
+	PlayerStateController playerStateManager;
+	public AudioClip finished;	// The sound that will be played when the player reaches the end of their journey.
+	bool sound_played = false;	// Used to check wether or not the finished sound has played
+	private RealSpace3D.RealSpace3D_AudioSource the_AudioSouce = null;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		the_AudioSouce = GameObject.Find ("First Person " +
+		                                  "Controller").GetComponent ("RealSpace3D_AudioSource") as RealSpace3D.RealSpace3D_AudioSource;
+		
+		if(the_AudioSouce == null)
+			Debug.LogError("theAudioSource isn't valid");
+
+		playerStateManager = gameObject.GetComponent <PlayerStateController> ();	// Get the player's current state
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (playerStateManager.GetState() == PlayerStateController.playerStates.finished && !sound_played) // If the playrer is in the finished state and the finish sound has not been played.
+		{
+			audio.Stop();	// Stop any the player is currently playing.
+			sound_played = true;	// Stops the sound being played more than once.
+			audio.PlayOneShot(finished); // Play the finish sound only once.
+			the_AudioSouce.rs3d_StopSound(1);
+		}
+	}
+}
---
+++ downloadThisBuild/Assets/Scripts/SlowmoLanding.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices; // Required for realspace3d
+
+// Script by Norbert Leskovics; modified by Tony Jarvis.
+// (AR)This script starts slow motion on the player and plays a sound.
+
+public class SlowmoLanding : MonoBehaviour 
+{
+	public AudioClip SoundFile;	// The sound that is played while the player is in slow motion
+	private RealSpace3D.RealSpace3D_AudioSource the_AudioSouce = null;
+	private GameObject windObject;
+
+	// Use this for initialization
+	void Start ()
+	{
+		the_AudioSouce = GameObject.Find ("First Person " +
+		                                  "Controller").GetComponent ("RealSpace3D_AudioSource") as RealSpace3D.RealSpace3D_AudioSource;
+		
+		if(the_AudioSouce == null)
+			Debug.LogError("theAudioSource isn't valid");
+
+		windObject = GameObject.Find ("Graphics");
+	}
+
+	// Runs when a collider touches this trigger.
+	void OnTriggerEnter( Collider other )
+	{
+		if (other.gameObject.GetComponent <PlayerMovement>() != null)
+		{
+			other.gameObject.GetComponent <PlayerMovement>().slowMo = true;
+			other.gameObject.audio.Stop ();
+			other.gameObject.audio.clip = SoundFile;
+			other.gameObject.audio.Play();
+		}
+		windObject.GetComponent<WindAmbience> ().windOn = false;
+		//the_AudioSouce.rs3d_LoadAudioClip("heartbeat");
+		//the_AudioSouce.rs3d_PlaySound (0);
+	}
+}
